0026859b2e3e88904659f0911ad82ee46c557b35
Javier-titan <javierhcortes@gmail.com>
2019-06-30 21:22:42 -0400

    agregado esquelo de task Handler

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 #! usr/bin/python3
+
 from fechas import TimeHandler
+from rutinas import TaskHandler
 
 timeHandler = TimeHandler()
 
---
+++ code/rutinas.py
@@ -0,0 +1,24 @@
+#! usr/bin/python3
+
+class TaskHandler():
+    
+    def archivoFlag_mod(self, value):
+        ''' Actualizar el valor de flag con valor 1 o 0'''
+        return 
+
+    def archivoIFCB_mod(self, tipo):
+        ''' Actualizar el archivo IFCB.cfg con 5 disitntos tipos '''
+        ''' Med. falsa - confg medicion. blanco. pre lavado- lavado'''
+        return 
+
+    def executeIFCB(self):
+        ''' Ejecutar el archivo ifcb.exe'''
+        return 
+    
+    def waitForFile(self, timeout):
+        ''' Esperar los archivos de salida, con timeout definido'''
+        return 
+
+    def transferir_ftp(self):
+        ''' Transferir archivos generados de cytoBot -> PC104'''
+        return
