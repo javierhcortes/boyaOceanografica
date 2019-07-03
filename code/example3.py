c00b4be7ec848cdb1d0ad5426ce249f5c5072e6d
javier <javierhcortes@gmail.com>
2019-07-03 17:11:54 -0400

    comiit de configPArser

--- code/example3.py
+++ code/example3.py
@@ -1,8 +1,39 @@
+#!/usr/bin/python3
 import configparser
-
-Config = configparser.ConfigParser()
-Config.read('example.ini')
+import os
 
 
-for (key,value) in Config['PRINCIPAL']:
-    print(key, value)
+def get_config(path):
+    """
+    Returns the config object
+    """
+    if not os.path.exists(path):
+        create_config(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+def get_setting(path, section, setting):
+    """
+    Print out a setting
+    """
+    config = get_config(path)
+    value = config.get(section, setting)
+    print(f'*En {section} CAMPO {setting} es {value}')
+    return value
+
+
+path = "settings.ini"
+
+l1 = get_setting(path,'DEFAULT', 'local_directory')
+
+# Config = configparser.ConfigParser()
+# Config.read('example.ini')
+
+# dict = {}
+
+# for key in Config['PRINCIPAL']:
+#     print(key)
+
+
