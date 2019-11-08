f97dc935e3b499565aafc68f460f60cf8801a612
j <local@pc104>
2019-11-08 19:00:46 -0300

    codigo de test local

--- code/main.py
+++ code/main.py
@@ -5,11 +5,15 @@ from rutinas import TaskHandler
 from iniReader import IniReader
 from logger import LogHandler
 import const
+import time
 
+import datetime
 
 if __name__ == "__main__":
 
 	#Inicio del logger
+	print("LOG =>Wait 30 segundos")
+	time.sleep(30)
 	logger = LogHandler()
 	logger.initMsg()
 
@@ -19,9 +23,53 @@ if __name__ == "__main__":
 
 	#TimeHandler, calcula la tarea en funcion del dia y la hora
 	timeHandler = TimeHandler()
+
 	task = timeHandler.calculateTask()
-	logger.anounceTask(task)
+	#logger.anounceTask(task)
 
 	#TaskHandler realiza las rutinas dependiendo de la tarea calculada anterior%
 	taskHandler = TaskHandler(iniReader)
-	taskHandler.doRutine(task)
+	#taskHandler.doRutine(task)
+
+	#Calculo de tiempo para tarea custom
+	horaActual = datetime.datetime.now().time().hour
+	minActual = datetime.datetime.now().time().minute
+	HoraPonderadaActual = horaActual*60 + minActual
+
+	horaMargen = 780 # 13:00am
+	if (HoraPonderadaActual < horaMargen):
+		print("LOG =>inicio Pruebas de MEDICION CUSTOM1...")
+		taskHandler.rutinaMedicion()
+		print("LOG =>fin Pruebas de MEDICION...")
+	elif (HoraPonderadaActual >= horaMargen):
+		print("LOG =>inicio Pruebas de LAVADO CUSTOM2...")
+		taskHandler.rutinaLavado()
+		print("LOG =>fin Pruebas de LAVADO...")
+	else:
+		print("LOG =>inicio Pruebas de LAVADO CUSTOM3...")
+		taskHandler.rutinaLavado()
+		print("LOG =>fin Pruebas de LAVADO...")
+
+	import subprocess
+	subprocess.call(["shutdown", "/s"])
+
+
+# if __name__ == "__main__verdadero":
+
+# 	#Inicio del logger
+# 	time.sleep(30)
+# 	logger = LogHandler()
+# 	logger.initMsg()
+
+# 	path = "settings.ini"
+# 	section = "PRINCIPAL"
+# 	iniReader = IniReader(path, section)
+
+# 	#TimeHandler, calcula la tarea en funcion del dia y la hora
+# 	timeHandler = TimeHandler()
+# 	task = timeHandler.calculateTask()
+# 	logger.anounceTask(task)
+
+# 	#TaskHandler realiza las rutinas dependiendo de la tarea calculada anterior%
+# 	taskHandler = TaskHandler(iniReader)
+# 	taskHandler.doRutine(task)
