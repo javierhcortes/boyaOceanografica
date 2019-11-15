d130dc1bb27c6c4fe816b19214f9241e43dd8c33
j <local@pc104>
2019-11-15 17:40:34 -0300

    Modificacion rutina general. apagado despues de 5min encendido

--- code/main.py
+++ code/main.py
@@ -6,8 +6,9 @@ from iniReader import IniReader
 from logger import LogHandler
 import const
 import time
+import subprocess
 
-import datetime
+#import datetime
 
 if __name__ == "__main__":
 
@@ -25,36 +26,19 @@ if __name__ == "__main__":
 	timeHandler = TimeHandler()
 
 	task = timeHandler.calculateTask()
-	#logger.anounceTask(task)
+	logger.anounceTask(task)
 
 	#TaskHandler realiza las rutinas dependiendo de la tarea calculada anterior%
 	taskHandler = TaskHandler(iniReader)
-	#taskHandler.doRutine(task)
-
-	#Calculo de tiempo para tarea custom
-	horaActual = datetime.datetime.now().time().hour
-	minActual = datetime.datetime.now().time().minute
-	HoraPonderadaActual = horaActual*60 + minActual
-
-	horaMargen = 780 # 13:00am
-	if (HoraPonderadaActual < horaMargen):
-		print("LOG =>inicio Pruebas de MEDICION CUSTOM1...")
-		taskHandler.rutinaMedicion()
-		print("LOG =>fin Pruebas de MEDICION...")
-	elif (HoraPonderadaActual >= horaMargen):
-		print("LOG =>inicio Pruebas de LAVADO CUSTOM2...")
-		taskHandler.rutinaLavado()
-		print("LOG =>fin Pruebas de LAVADO...")
-	else:
-		print("LOG =>inicio Pruebas de LAVADO CUSTOM3...")
-		taskHandler.rutinaLavado()
-		print("LOG =>fin Pruebas de LAVADO...")
-
-	import subprocess
+	taskHandler.doRutine(task)
+	
+	print("LOG => Wait 5min ")
+	time.sleep(60*5)
+	#Se demora un minuto en apagarse
 	subprocess.call(["shutdown", "/s"])
 
 
-# if __name__ == "__main__verdadero":
+# if __name__ == "__main__pruebas":
 
 # 	#Inicio del logger
 # 	time.sleep(30)
@@ -68,8 +52,24 @@ if __name__ == "__main__":
 # 	#TimeHandler, calcula la tarea en funcion del dia y la hora
 # 	timeHandler = TimeHandler()
 # 	task = timeHandler.calculateTask()
-# 	logger.anounceTask(task)
+# #Calculo de tiempo para tarea custom
+# horaActual = datetime.datetime.now().time().hour
+# minActual = datetime.datetime.now().time().minute
+# HoraPonderadaActual = horaActual*60 + minActual
 
-# 	#TaskHandler realiza las rutinas dependiendo de la tarea calculada anterior%
-# 	taskHandler = TaskHandler(iniReader)
-# 	taskHandler.doRutine(task)
+# horaMargen = 780 # 13:00am
+# if (HoraPonderadaActual < horaMargen):
+# 	print("LOG =>inicio Pruebas de MEDICION CUSTOM1...")
+# 	taskHandler.rutinaMedicion()
+# 	print("LOG =>fin Pruebas de MEDICION...")
+# elif (HoraPonderadaActual >= horaMargen):
+# 	print("LOG =>inicio Pruebas de LAVADO CUSTOM2...")
+# 	taskHandler.rutinaLavado()
+# 	print("LOG =>fin Pruebas de LAVADO...")
+# else:
+# 	print("LOG =>inicio Pruebas de LAVADO CUSTOM3...")
+# 	taskHandler.rutinaLavado()
+# 	print("LOG =>fin Pruebas de LAVADO...")
+
+# import subprocess
+# subprocess.call(["shutdown", "/s"])
